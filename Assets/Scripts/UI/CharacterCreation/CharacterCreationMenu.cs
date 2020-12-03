da2381cb49f82a2930637c2e54ea0517f02f3ae6
Kristýna Pavlátová <60941311+KristynaPavlatova@users.noreply.github.com>
2020-12-03 15:17:32 +0100

    First commit

---
+++ Assets/Scripts/UI/CharacterCreation/CharacterCreationMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;//for using Events
+
+public class CharacterCreationMenu : MonoBehaviour
+{
+    public GameObject Character;//to store the created character
+    //insert the gameObjects that are responsible for switching the character parts, not the actual character parts
+    public List<CharacterChanger> changerOfCharacerPart = new List<CharacterChanger>();
+    public static event Action CharacterSubmited;
+    //--------------------------------------------------------------------------------------------------------------------------------
+
+    private void Start()
+    {
+        Debug.Log("CharacterCreationMenu: RandomizeCharacter();");
+        SetDefaultCharacter();
+    }
+    //---------------------------------------------------------------------------------
+    // SET DEFAULT LOOK FOR CHARACTER IN CHAR. CREATION MENU
+    //---------------------------------------------------------------------------------
+    public void SetDefaultCharacter()//before changing it based on user input
+    {
+        foreach (CharacterChanger changer in changerOfCharacerPart)//go through the list of changers and call their Randomize method
+        {
+            changer.switchArtSets();//change the art set of the changer
+            changer.SetToFirstSprite();
+            Debug.Log("CharacterCreationMenu: changer.Randomize();");
+        }
+    }
+
+    public void SubmitCharacter()
+    {
+        Debug.Log("CharacterCreationMenu: Character submited >> GameManager");
+        CharacterSubmited();//for gameManager that character is done + AudioManager to play confirm sound
+    }
+}
---
+++ Assets/Scripts/UI/WebsiteForm/InputFieldTransfer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;//for using Events
+
+public class InputFieldTransfer : MonoBehaviour
+{
+    [HideInInspector]
+    public string Name;
+    [SerializeField]
+    public GameObject inputFieldName;
+    public GameObject textDisplayName;
+    
+    [HideInInspector]
+    public string Age;
+    [SerializeField]
+    public GameObject inputFieldAge;
+    public GameObject textDisplayAge;
+
+    [HideInInspector]
+    public string Race;
+    [SerializeField]
+    public GameObject inputFieldRace;
+    public GameObject textDisplayRace;
+
+    [HideInInspector]
+    public string Gender;
+    [SerializeField]
+    public GameObject inputFieldGender;
+    public GameObject textDisplayGender;
+
+    public static event Action ContinuePressed;
+    //--------------------------------------------------------------------------------------------------------------------------------
+
+    public void Continue()//continue to CharacterCreation part
+    {
+        StoreFieldsInfo();
+        ContinuePressed();//EVENT
+        Debug.Log("InputFieldTransfer: ContinuePressed event");
+    }
+    private void StoreFieldsInfo()
+    {
+        StoreName();
+        StoreAge();
+    }
+    public void StoreName()
+    {
+        Name = inputFieldName.GetComponent<Text>().text;//what ever is written in the inputFieldName >> put it into the Name variable
+        textDisplayName.GetComponent<Text>().text = Name;//put the string Name into the text that gets displayed
+        //Debug.Log("StoreName");
+    }
+    public void StoreAge()
+    {
+        Age = inputFieldAge.GetComponent<Text>().text;//what ever is written in the inputFieldName >> put it into the Name variable
+        textDisplayAge.GetComponent<Text>().text = Age;//put the string Name into the text that gets displayed
+        //Debug.Log("StoreAge");
+    }
+}
